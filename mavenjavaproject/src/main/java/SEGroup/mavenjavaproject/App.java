d959f7bba12461c03c76324b1a4ca7f3bc05f091
vaishnavi <vaishnavib@gmail.com>
2024-11-20 21:35:08 +0530

    maven web push

---
+++ mavenjavaproject/src/main/java/SEGroup/mavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.mavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World this is vaishnavi 22bd1a664a" );
+    }
+}
---
+++ mavenwebproject/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! iam vaishnavi </h2>
+</body>
+</html>
